0b1ab3c7917e56de3dd203a7a130bcde59135717
Jesus Pardo <jpardo@growthaccelerationpartners.com>
2026-07-07 14:16:30 -0400

    Configure retries and update setup project options
    Added retries configuration and enhanced setup project settings.

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,6 +15,7 @@ const testDir = defineBddConfig({
 
 export default defineConfig({
   testDir,
+  retries: 1,
   reporter: [
     [ 'line' ],
     [ 'allure-playwright', { resultsDir: 'test-results/allure-results' } ],
@@ -32,7 +33,13 @@ export default defineConfig({
     {
       name: 'setup',
       testDir: 'tests/e2e',
-      testMatch: /.*\.setup\.ts/
+      testMatch: /.*\.setup\.ts/,
+      use: {
+        ...devices['Desktop Chrome'],
+        trace: 'on',
+        video: 'on',
+        screenshot: 'on',
+      }
     },
     {
       name: 'chromium',
